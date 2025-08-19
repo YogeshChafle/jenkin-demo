26c18064286f53a4e492027fe7dd08ab7da6b943
Yogesh Chafle <yogeshchafle9049@gmail.com>
2025-08-19 12:02:55 +0530

    Initail Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("This is Java Program");
+        System.out.println(new Date());
+        System.out.println("Done");
+    }
+}
